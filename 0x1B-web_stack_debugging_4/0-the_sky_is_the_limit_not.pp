12c138d310ade4f806df414bbcbad0e4e4cd4a22
Shaun2003 <tshimangadzoshaun21@gmail.com>
2023-11-16 14:29:58 -0800

    yy

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,17 @@
-# Puppet script
-exec { '/usr/bin/env sed -i s/15/1000/ /etc/default/nginx': }
--> exec { '/usr/bin/env service nginx restart': }
+# increases the amount of traffic an Nginx service can handle.
+
+# increase the ULIMIT of the default file
+exec { 'fix--for -nginx':
+# modify the ULIMIT value
+command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+# specify the path for the sed command
+path => '/usr/local/bin/:/bin/',
+}
+
+# restart Nginx
+exec { 'nginx-restart':
+# restart Nginx service
+command => '/etc/init.d/nginx restart',
+# specify the path for the init.d script
+path => '/etc/init.d/'
+}
